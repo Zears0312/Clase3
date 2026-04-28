5011f2917f2ae98abf47e5fbfe6c7531870b2123
franco sosa <fransosa953@gmail.com>
2026-04-27 22:10:29 -0300

    Trabajo en clases2

---
+++ Trabajoenclases/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b= 3;
+        Console.WriteLine(a+b);
+        Console.WriteLine(a-b);
+        Console.WriteLine(a*b);
+        Console.WriteLine(a/b);
+        Console.WriteLine(a%b);
+        double resultado = (double) a/b; //3.333.. 
+        // conversion de string a numero
+        string entrada = "42";
+        int numero = int.Parse(entrada);
+        //Try parse conversion segura 
+        if(double.TryParse("3.14", out double pi))
+            Console.WriteLine($"Pi = {pi}");
+        
+        // Asignacion compuesta
+        int puntos = 100;
+        puntos += 50;
+        puntos *= 2;
+    }
+}
